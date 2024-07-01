4adedcdf34fbf1ced451ea374ccbe16a1d3ad594
Muhammet Andiç <muhammet.andic@gmail.com>
2024-07-01 14:28:24 +0300

    dark - light mode implemented

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,13 @@
 <script setup lang="ts">
 import { RouterLink, RouterView } from 'vue-router'
+import { ref } from 'vue'
+
+const darkMode = ref(localStorage.getItem('darkMode') || false)
+const toggleTheme = () => {
+  darkMode.value = !darkMode.value
+  document.documentElement.setAttribute('data-theme', darkMode.value ? 'dark' : 'light')
+  localStorage.setItem('darkMode', darkMode.value)
+}
 </script>
 
 <template>
@@ -8,6 +16,7 @@ import { RouterLink, RouterView } from 'vue-router'
       <nav>
         <RouterLink to="/">Home</RouterLink>
         <RouterLink to="/about">About</RouterLink>
+        <span @click="toggleTheme">Toggle {{ darkMode ? 'dark' : 'light' }} </span>
       </nav>
     </div>
   </header>
