69a05dee4f018e8d04a66a1b7d8b36e52282caad
Muhammet Andiç <muhammet.andic@gmail.com>
2024-07-01 16:10:29 +0300

    dark-light mode icons added

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,13 @@
 <script setup lang="ts">
 import { RouterLink, RouterView } from 'vue-router'
 import { ref } from 'vue'
+import SunIcon from './components/icons/SunIcon.vue'
+import MoonIcon from './components/icons/MoonIcon.vue'
 
-const darkMode = ref(localStorage.getItem('darkMode') || false)
+const isDark = localStorage.getItem('darkMode') === 'true' ? true : false
+document.documentElement.setAttribute('data-theme', isDark ? 'dark' : 'light')
+
+const darkMode = ref(isDark)
 const toggleTheme = () => {
   darkMode.value = !darkMode.value
   document.documentElement.setAttribute('data-theme', darkMode.value ? 'dark' : 'light')
@@ -11,15 +16,43 @@ const toggleTheme = () => {
 </script>
 
 <template>
-  <header>
-    <div class="wrapper">
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-        <span @click="toggleTheme">Toggle {{ darkMode ? 'dark' : 'light' }} </span>
-      </nav>
-    </div>
-  </header>
+  <div class="app">
+    <header>
+      <div class="wrapper">
+        <nav>
+          <RouterLink to="/">Home</RouterLink>
+          <RouterLink to="/about">About</RouterLink>
+        </nav>
+        <SunIcon v-if="darkMode" @click="toggleTheme" class="icon" width="24px" height="24px" />
+        <MoonIcon v-else @click="toggleTheme" class="icon" width="24px" height="24px" />
+      </div>
+      <hr />
+    </header>
 
-  <RouterView />
+    <RouterView />
+  </div>
 </template>
+
+<style scoped>
+.app {
+  max-width: 960px;
+  margin: 0 auto;
+}
+
+.wrapper {
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+}
+
+.icon {
+  cursor: pointer;
+}
+
+nav a {
+  font-weight: bold;
+  text-decoration: none;
+  color: var(--primary-color);
+  margin: 0 8px 0 0;
+}
+</style>
